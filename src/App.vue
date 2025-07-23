3598e9e1be91dd99a5237ca7d77054bf2167769c
rizkys-code <2311500173@student.budiluhur.ac.id>
2025-07-23 22:32:47 +0700

    fix: image dummy in project

--- src/App.vue
+++ src/App.vue
@@ -31,13 +31,13 @@ const experiences = ref([
 // Data untuk bagian Skills
 const skills = ref([
   {
-    // Untuk ikon yang berupa SVG, kita simpan kode SVG-nya langsung
+    
     icon: '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256"><g fill="none"><rect width="256" height="256" fill="#e14e1d" rx="60"/><path fill="#fff" d="m48 38l8.61 96.593h110.71l-3.715 41.43l-35.646 9.638l-35.579-9.624l-2.379-26.602H57.94l4.585 51.281l65.427 18.172l65.51-18.172l8.783-98.061H85.824l-2.923-32.71h122.238L208 38z"/><path fill="#ebebeb" d="M128 38H48l8.61 96.593H128v-31.938H85.824l-2.923-32.71H128zm0 147.647l-.041.014l-35.579-9.624l-2.379-26.602H57.94l4.585 51.281l65.427 18.172l.049-.014z"/></g></svg>',
     name: 'HTML',
     description: 'Keterampilan dalam membuat struktur halaman web yang semantik dan rapi.'
   },
   {
-    // Untuk ikon dari Font Awesome, kita simpan nama kelasnya
+    
     iconClass: 'fa-brands fa-css3-alt',
     name: 'CSS',
     description: 'Kemampuan untuk mendesain dan menata halaman web menjadi lebih menarik dan responsif.'
@@ -53,12 +53,12 @@ const skills = ref([
 const projects = ref([
   {
     // Pastikan path ini benar menuju gambar di folder public/img/
-    image: '/img/Screenshot 2025-06-15 175555.png',
+    image: '/img/LAB.png',
     title: 'Website Informasi Masjid At-Taqwa',
     description: 'Membuat website profil dan sistem informasi untuk Masjid At-Taqwa menggunakan HTML, CSS, dan PHP.'
   },
   {
-    image: '/img/Screenshot 2025-06-15 181909.png',
+    image: '/img/Martabak.png',
     title: 'Desain UI/UX Aplikasi Mobile',
     description: 'Merancang antarmuka pengguna untuk aplikasi mobile pemesanan makanan dengan Figma.'
   }
